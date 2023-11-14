d6e9b378583fb96b58458bf55ba4e684921aeb8f
HELIOPROGAM <136519630+HELIOPROGAM@users.noreply.github.com>
2023-11-14 20:04:19 -0300

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,4 +1,18 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA21y0CO0RtMGElW9NxIq6wZ5ak162Liaw",
+  authDomain: "testekwitter.firebaseapp.com",
+  databaseURL: "https://testekwitter-default-rtdb.firebaseio.com",
+  projectId: "testekwitter",
+  storageBucket: "testekwitter.appspot.com",
+  messagingSenderId: "364995332721",
+  appId: "1:364995332721:web:0de8c39165ffdfe29269d0",
+  measurementId: "G-09MT67BKZQ"
+};
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
 // Defina a função.
+
 function addUser() {
 
  // Obtenha o nome do usuário da input box e armazene dentro de uma variável
